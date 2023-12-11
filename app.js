51288ba1bdcb8d8ca77253348c05cf1da381f2cb
sema-demir <semademir0505@gmail.com>
2023-12-11 12:53:24 +0300

    eklemeler yapıldı

--- app.js
+++ app.js
@@ -52,7 +52,10 @@ cardArray.sort(() => 0.5 - Math.random())
 
 
 const gridDisplay = document.querySelector("#grid")
+let score = document.getElementById("score")
 let cardChosen = []
+let cardChosenID = []
+const cardWon = []
 
 function createBoard(){
     for(let i = 0; i<cardArray.length; i++){
@@ -65,8 +68,40 @@ function createBoard(){
 }
 createBoard()
 
+function checkMatch(){
+  const cards = document.querySelectorAll("#grid img")
 
-function flipcard() {
+  const optionOneID = cardChosenID[0]
+  const optionTwoID = cardChosenID[1]
+
+  if(cardChosen[0] === cardChosen[1]){
+    cards[optionOneID].setAttribute("src", "images/white.png")
+    cards[optionTwoID].setAttribute("src", "images/white.png")
+    cards[optionOneID].removeEventListener("click", flipCard)
+    cards[optionTwoID].removeEventListener("click", flipCard)
+    cardWon.push(cardChosen)
+  }else {
+    cards[optionOneID].setAttribute("src", "images/blank.png")
+    cards[optionTwoID].setAttribute("src", "images/blank.png")
+  }
+score.textContent = cardWon.length
+cardChosen = []
+cardChosenID = []
+
+if(cardWon.length == cardArray.length/2){
+  score.innerHTML = "Tebrikler Kazandınız..."
+}
+
+}
+
+
+function flipCard() {
     const cardID = this.getAttribute("data-id")
-    cardChosen.push(card)
+    cardChosen.push(cardArray[cardID].name)
+    cardChosenID.push(cardID)
+    this.setAttribute("src", cardArray[cardID].img)
+
+    if(cardChosen.length == 2) {
+      setTimeout(checkMatch, 500)
+    }
 }
